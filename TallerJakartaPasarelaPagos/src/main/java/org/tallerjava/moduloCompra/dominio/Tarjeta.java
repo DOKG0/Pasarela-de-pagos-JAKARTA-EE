eb0ad1fe8afc82a9b9a98a18e969cebd70e8b7fb
Michel Cufre <137198073+MichelCufre@users.noreply.github.com>
2025-05-13 11:23:58 -0300

    feat: proyecto base

---
+++ TallerJakartaPasarelaPagos/src/main/java/org/tallerjava/moduloCompra/dominio/Tarjeta.java
@@ -0,0 +1,9 @@
+package org.tallerjava.moduloComercio.dominio;
+
+import java.time.LocalDate;
+
+public class Tarjeta {
+    private Integer nro;
+    private String marca;
+    private LocalDate fechaVto;
+}
