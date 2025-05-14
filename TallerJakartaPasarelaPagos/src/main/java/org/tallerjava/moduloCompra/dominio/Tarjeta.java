f205721b4bd6c610a859cf9f90d56079dce194b6
Michel Cufre <137198073+MichelCufre@users.noreply.github.com>
2025-05-14 01:36:50 -0300

    Delete TallerJakartaPasarelaPagos directory

--- TallerJakartaPasarelaPagos/src/main/java/org/tallerjava/moduloCompra/dominio/Tarjeta.java
+++
@@ -1,9 +0,0 @@
-package org.tallerjava.moduloComercio.dominio;
-
-import java.time.LocalDate;
-
-public class Tarjeta {
-    private Integer nro;
-    private String marca;
-    private LocalDate fechaVto;
-}
